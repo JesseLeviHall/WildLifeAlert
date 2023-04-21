1fb28d47acb805a81b7b03f19175aa8e056ee09f
jesse h <jesselevihall@gmail.com>
2023-04-21 16:11:01 -0600

    redis client

--- wlaserver/src/app.ts
+++ wlaserver/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import logger from "morgan";
 import * as path from "path";
 import helmet from "helmet";
+import { createClient } from 'redis';
 
 import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
 
@@ -10,6 +11,15 @@ import { api } from "./routes/api";
 
 // Create Express server
 export const app = express();
+
+// Redis
+const client = createClient({
+    password: 'RaW2XeXJ2nQQCi7VXBtSFxBCDcRBKPEI',
+    socket: {
+        host: 'redis-19415.c289.us-west-1-2.ec2.cloud.redislabs.com',
+        port: 19415
+    }
+});
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
