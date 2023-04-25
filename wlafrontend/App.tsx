9a6d77a3fa8dfeeb8b39931ee4a12fdb6b583cec
jesse h <jesselevihall@gmail.com>
2023-04-25 17:21:43 -0600

    tanstack install

--- wlafrontend/App.tsx
+++ wlafrontend/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import StackNavigator from './StackNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function App() {
   return (
