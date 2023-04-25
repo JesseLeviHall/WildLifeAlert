85bfbf71ebf4e5a921b3d6d7355396667e5ccc58
jesse h <jesselevihall@gmail.com>
2023-04-25 17:29:59 -0600

    start queries

--- wlafrontend/components/componentOne.tsx
+++ wlafrontend/components/componentOne.tsx
@@ -3,6 +3,12 @@ import { Text } from 'react-native'
 
 type Props = {}
 
+function wait(duration: number): void{
+  return new Promise(resolve => {
+    setTimeout(resolve, duration)
+  })
+}
+
 const componentOne = (props: Props) => {
   return (
     <Text>componentOne</Text>
