3d832816d885c709d73aff49f4e41d3e227d9b09
jesse h <jesselevihall@gmail.com>
2023-04-25 17:27:02 -0600

    react query initialize

--- wlafrontend/App.tsx
+++ wlafrontend/App.tsx
@@ -3,10 +3,14 @@ import StackNavigator from './StackNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
     <NavigationContainer>
+      <QueryClientProvider client={queryClient}>
     		<StackNavigator />
+        </QueryClientProvider>
     </NavigationContainer>
   );
 }
