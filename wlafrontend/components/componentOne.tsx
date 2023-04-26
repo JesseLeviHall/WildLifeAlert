6079cdc30e77fedf5b0a5b8c01f4f9e22223ab63
jesse h <jesselevihall@gmail.com>
2023-04-26 09:18:56 -0600

    note

--- wlafrontend/components/componentOne.tsx
+++ wlafrontend/components/componentOne.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import { Text } from 'react-native'
+import { useQuery, useMutation } from '@tanstack/react-query'
+
+const posts = {
+  1: { id: 1, title: 'First Post', content: 'Hello!' },
+  2: { id: 2, title: 'Second Post', content: 'More text' },
+}
 
 type Props = {}
 
-function wait(duration: number): void{
+function wait(duration: number): unknown{
   return new Promise(resolve => {
     setTimeout(resolve, duration)
   })
 }
 
 const componentOne = (props: Props) => {
+  const postsQuery = useQuery({
+    queryKey: ['getposts'],
+    queryFn: async () => {
+      await wait(1000)
+      return posts
+    }
+  })
+
+  if(postsQuery.isLoading) return <Text>Loading</Text>
+  if(postsQuery.isError) return <Text>{JSON.stringify(postsQuery.error)}</Text>
+
   return (
     <Text>componentOne</Text>
   )
