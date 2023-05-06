ff245f280555934c56dca458cf0dd032639a80ca
jesse h <jesselevihall@gmail.com>
2023-05-06 09:20:11 -0600

    set up map component

--- wlafrontend/screens/PublicMap.tsx
+++ wlafrontend/screens/PublicMap.tsx
@@ -1,21 +1,105 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useLayoutEffect } from 'react';
+import { Text, RefreshControl, View, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Dimensions } from 'react-native';
+import { Motion } from '@legendapp/motion';
+import { useQuery } from '@tanstack/react-query/build/lib';
+import { Appbar, FAB } from 'react-native-paper';
+import { useRefreshByUser } from '../hooks/useRefreshByUser';
+import { useNavigation, NavigationProp } from '@react-navigation/native';
+import SpinnerComp from '../components/Spinner';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
+const BOTTOM_APPBAR_HEIGHT = 80;
+const MEDIUM_FAB_HEIGHT = 56;
 
 const screenHeight = Dimensions.get('window').height;
 
-type Props = {}
+type RootStackParamList = {
+	Home: undefined;
+};
+type HomeScreenNavigationProp = NavigationProp<RootStackParamList, 'Home'>;
+type Props = {
+	navigation: HomeScreenNavigationProp;
+};
 
 const PublicMap = (props: Props) => {
-  return (
-    <LinearGradient
+	const { bottom } = useSafeAreaInsets();
+	const navigation = useNavigation<HomeScreenNavigationProp>();
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			headerShown: false,
+		});
+	});
+	return (
+		<LinearGradient
 			style={{ height: screenHeight }}
 			colors={['#33fff2', '#4deefc', '#a6cedd']}>
-     <Text>Public Map</Text>
-    </LinearGradient>
-  )
-}
+			<Appbar.Header className=''>
+				<Appbar.BackAction
+					onPress={() => {
+						navigation.navigate('Home');
+					}}
+				/>
+				<Appbar.Content title='Live Map' />
+				<Appbar.Action icon='map' onPress={() => {}} />
+				<Appbar.Action icon='more' onPress={() => {}} />
+			</Appbar.Header>
+      <Motion.View className='flex-1 align-middle justify-center' initial={{ x: -300, scale: 0, opacity: 0 }}
+					animate={{ x: 0, scale: 1, opacity: 1 }}
+					transition={{
+						x: {
+							type: 'spring',
+							damping: 20,
+							stiffness: 800,
+							mass: 2,
+						},
+						opacity: {
+							type: 'tween',
+							duration: 1000,
+						},
+					}}>
+			<Text className='flex-col align-middle'>Public Map Will Go Here</Text></Motion.View>
+			<Appbar
+				style={[
+					styles.bottom,
+					{
+						height: BOTTOM_APPBAR_HEIGHT + bottom,
+						backgroundColor: '#bad1e8',
+					},
+				]}
+				safeAreaInsets={{ bottom }}>
+				<Appbar.Action icon='archive' onPress={() => {}} />
+				<Appbar.Action icon='email' onPress={() => {}} />
+				<Appbar.Action icon='label' onPress={() => {}} />
+				<Appbar.Action icon='delete' onPress={() => {}} />
+				<FAB
+					mode='flat'
+					size='medium'
+					icon='plus'
+					onPress={() => {}}
+					style={[
+						styles.fab,
+						{ top: (BOTTOM_APPBAR_HEIGHT - MEDIUM_FAB_HEIGHT) / 2 },
+					]}
+				/>
+			</Appbar>
+		</LinearGradient>
+	);
+};
 
-export default PublicMap
+export default PublicMap;
+
+const styles = StyleSheet.create({
+	bottom: {
+		backgroundColor: 'aquamarine',
+		position: 'absolute',
+		left: 0,
+		right: 0,
+		bottom: 0,
+	},
+	fab: {
+		position: 'absolute',
+		right: 16,
+	},
+});
