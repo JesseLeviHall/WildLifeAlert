369efe1826def566be92faf49d1692c0a1909584
jesse h <jesselevihall@gmail.com>
2023-05-25 23:06:55 -0600

    set location is in a good spot

--- wlafrontend/screens/Resources.tsx
+++ wlafrontend/screens/Resources.tsx
@@ -1,38 +1,61 @@
-import React from 'react';
-import { View } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import { Dimensions } from 'react-native';
-import { Link, Text, VStack, Image } from 'native-base';
+import React from "react";
+import { View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { Appbar } from "react-native-paper";
+import { Dimensions } from "react-native";
+import { Link, Text, VStack, Image } from "native-base";
+import { useNavigation, NavigationProp } from "@react-navigation/native";
 
-const screenHeight = Dimensions.get('window').height;
+const screenHeight = Dimensions.get("window").height;
 
-type Props = {};
+type RootStackParamList = {
+  Home: undefined;
+};
+type HomeScreenNavigationProp = NavigationProp<RootStackParamList, "Home">;
+type Props = {
+  navigation: HomeScreenNavigationProp;
+};
 
 const Resources = (props: Props) => {
-	return (
-		<LinearGradient
-			style={{ height: screenHeight }}
-			colors={['#0DE69A', '#71D1C7', '#99BBE3']}>
-			<VStack space={2} justifyContent='center' alignItems='center' safeAreaTop>
-				<Image
-					width={40}
-					height={20}
-					resizeMode='cover'
-					source={{
-						uri: 'https://ahnow.org/images/weblink_small.png',
-					}}
-					alt='animal help now.org'
-				/>
-				<Link href='https://ahnow.org' isExternal>
-					<Text>Visit </Text>
-					<Text className=' text-blue-500'>AnimalHelpNow.org </Text>
-				</Link>
-				<Text className='mx-16'>
-					to locate wildlife rescue organizations near you
-				</Text>
-			</VStack>
-		</LinearGradient>
-	);
+  const navigation = useNavigation<HomeScreenNavigationProp>();
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
+  return (
+    <LinearGradient
+      style={{ height: screenHeight }}
+      colors={["#0DE69A", "#71D1C7", "#99BBE3"]}
+    >
+      <Appbar.Header className="">
+        <Appbar.BackAction
+          onPress={() => {
+            navigation.navigate("Home");
+          }}
+        />
+        <Appbar.Content title="Resources" />
+      </Appbar.Header>
+      <VStack space={2} justifyContent="center" alignItems="center" safeAreaTop>
+        <Image
+          width={40}
+          height={20}
+          resizeMode="cover"
+          source={{
+            uri: "https://ahnow.org/images/weblink_small.png",
+          }}
+          alt="animal help now.org"
+        />
+        <Link href="https://ahnow.org" isExternal>
+          <Text>Visit </Text>
+          <Text className=" text-blue-500">AnimalHelpNow.org </Text>
+        </Link>
+        <Text className="mx-16">
+          to locate wildlife rescue organizations near you
+        </Text>
+      </VStack>
+    </LinearGradient>
+  );
 };
 
 export default Resources;
