4b40f3cb6a81649fd2aa7ef07228e709258403f5
jesse h <jesselevihall@gmail.com>
2023-05-28 17:25:32 -0600

    animations on toast

--- wlafrontend/components/SuccessToast.tsx
+++ wlafrontend/components/SuccessToast.tsx
@@ -1,14 +1,41 @@
-import React from "react";
+import * as React from "react";
 import { View, Text } from "react-native";
+import { Motion } from "@legendapp/motion";
+import { styled } from "nativewind";
+
+const MotionView = styled(View);
 
 type Props = {
   message: string;
 };
 
 const SuccessToast = (props: Props) => {
+  const [value, setValue] = React.useState(false);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((prev) => !prev);
+    }, 120); // Adjust this interval for faster or slower bounces
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: "flex-end" }}>
-      <View
+    <MotionView style={{ flex: 1, justifyContent: "flex-end" }}>
+      <Motion.View
+        initial={{ y: 0, opacity: 0 }}
+        animate={{ y: value ? 50 : -50, opacity: 1 }} // Adjust these values for bigger or smaller bounces
+        exit={{ opacity: 0 }}
+        transition={{
+          y: {
+            type: "spring",
+            damping: 30,
+            stiffness: 30,
+          },
+          opacity: {
+            type: "tween",
+            duration: 500,
+          },
+        }}
         style={{
           backgroundColor: "#bad1e8",
           opacity: 0.5,
@@ -21,8 +48,8 @@ const SuccessToast = (props: Props) => {
         <Text style={{ fontSize: 20, fontWeight: "bold" }}>
           {props.message}
         </Text>
-      </View>
-    </View>
+      </Motion.View>
+    </MotionView>
   );
 };
 
