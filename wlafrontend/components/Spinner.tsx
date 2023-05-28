92fade1bc134b96476df83befbdf603784627b09
jesse h <jesselevihall@gmail.com>
2023-05-28 17:33:54 -0600

    center spinner on screen

--- wlafrontend/components/Spinner.tsx
+++ wlafrontend/components/Spinner.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { HStack, Spinner } from "native-base";
+import { HStack, Spinner, View } from "native-base";
+import { LinearGradient } from "expo-linear-gradient";
+import { Dimensions } from "react-native";
 
 type Props = {};
 
 const SpinnerComp = (props: Props) => {
+  const screenHeight = Dimensions.get("window").height;
   return (
-    <HStack space={2} alignItems="center">
-      <Spinner accessibilityLabel="Loading" size="lg" />
-    </HStack>
+    <LinearGradient
+      style={{ height: screenHeight }}
+      colors={["#0DE69A", "#71D1C7", "#99BBE3"]}
+    >
+      <View className="flex-1 align-middle justify-center">
+        <Spinner accessibilityLabel="Loading" size="lg" />
+      </View>
+    </LinearGradient>
   );
 };
 
