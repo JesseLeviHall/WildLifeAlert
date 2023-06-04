534e37c646ce7663dab492faa3d623f9f7b7e19e
jesse h <jesselevihall@gmail.com>
2023-06-04 12:52:10 -0600

    animated gradient on homescreen

--- wlafrontend/components/HomeNavBot.tsx
+++ wlafrontend/components/HomeNavBot.tsx
@@ -1,104 +1,124 @@
-import React from 'react';
-import { View } from 'react-native';
-import { TouchableRipple, Surface } from 'react-native-paper';
-import { useNavigation, NavigationProp } from '@react-navigation/core';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { Box, HStack } from 'native-base';
-
+import React from "react";
+import { Dimensions, StyleSheet } from "react-native";
+import { TouchableRipple, Surface } from "react-native-paper";
+import { useNavigation, NavigationProp } from "@react-navigation/core";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { Box, HStack } from "native-base";
 
 type RootStackParamList = {
-	Home: undefined;
-	PublicMap: undefined;
-	SendForHelp: undefined;
-	About: undefined;
-	RescuerLogin: undefined;
-	Resources: undefined;
+  Home: undefined;
+  PublicMap: undefined;
+  SendForHelp: undefined;
+  About: undefined;
+  RescuerLogin: undefined;
+  Resources: undefined;
 };
-type HomeScreenNavigationProp = NavigationProp<RootStackParamList, 'Home'>;
+type HomeScreenNavigationProp = NavigationProp<RootStackParamList, "Home">;
 type Props = {
-	navigation: HomeScreenNavigationProp;
+  navigation: HomeScreenNavigationProp;
 };
+
+const screenWidth = Dimensions.get("window").width;
 
 const HomeNavBot = (props: Props) => {
-	const navigation = useNavigation<HomeScreenNavigationProp>();
-	return (
-    <Surface elevation={4}>
-		<HStack
-			space={3}
-			borderWidth='1'
-			borderColor='coolGray.300'
-			justifyContent='center'
-			bg='#33fff2'>
-			<TouchableRipple
-				borderless={true}
-				underlayColor='#99BBE3'
-				onPress={() => navigation.navigate('RescuerLogin')}>
-				<Box
-					p='2'
-					alignItems='center'
-					_text={{
-						fontSize: 'sm',
-						fontWeight: 'medium',
-						letterSpacing: 'lg',
-					}}>
-					<MaterialCommunityIcons name='launch' size={24} />
-					{'Rescuers'}
-				</Box>
-			</TouchableRipple>
+  const navigation = useNavigation<HomeScreenNavigationProp>();
+  return (
+    <Surface elevation={4} style={styles.container}>
+      <HStack
+        space={3}
+        borderWidth="1"
+        borderColor="coolGray.300"
+        justifyContent="center"
+        bg="#33fff2"
+      >
+        <TouchableRipple
+          borderless={true}
+          underlayColor="#99BBE3"
+          onPress={() => navigation.navigate("RescuerLogin")}
+        >
+          <Box
+            p="2"
+            alignItems="center"
+            _text={{
+              fontSize: "sm",
+              fontWeight: "medium",
+              letterSpacing: "lg",
+            }}
+          >
+            <MaterialCommunityIcons name="launch" size={24} />
+            {"Rescuers"}
+          </Box>
+        </TouchableRipple>
 
-			<TouchableRipple
-				borderless={true}
-				rippleColor='#99BBE3'
-				onPress={() => navigation.navigate('PublicMap')}>
-				<Box
-					p='2'
-					alignItems='center'
-					_text={{
-						fontSize: 'sm',
-						fontWeight: 'medium',
-						letterSpacing: 'lg',
-					}}>
-					<MaterialCommunityIcons name='latitude' size={24} />
-					{'Live Map'}
-				</Box>
-			</TouchableRipple>
+        <TouchableRipple
+          borderless={true}
+          rippleColor="#99BBE3"
+          onPress={() => navigation.navigate("PublicMap")}
+        >
+          <Box
+            p="2"
+            alignItems="center"
+            _text={{
+              fontSize: "sm",
+              fontWeight: "medium",
+              letterSpacing: "lg",
+            }}
+          >
+            <MaterialCommunityIcons name="earth" size={24} />
+            {"Live Map"}
+          </Box>
+        </TouchableRipple>
 
-			<TouchableRipple
-				borderless={true}
-				rippleColor='#99BBE3'
-				onPress={() => navigation.navigate('Resources')}>
-				<Box
-					p='2'
-					alignItems='center'
-					_text={{
-						fontSize: 'sm',
-						fontWeight: 'medium',
-						letterSpacing: 'lg',
-					}}>
-					<MaterialCommunityIcons name='magnify-expand' size={24} />
-					{'Resources'}{' '}
-				</Box>
-			</TouchableRipple>
+        <TouchableRipple
+          borderless={true}
+          rippleColor="#99BBE3"
+          onPress={() => navigation.navigate("Resources")}
+        >
+          <Box
+            p="2"
+            alignItems="center"
+            _text={{
+              fontSize: "sm",
+              fontWeight: "medium",
+              letterSpacing: "lg",
+            }}
+          >
+            <MaterialCommunityIcons name="magnify-expand" size={24} />
+            {"Resources"}{" "}
+          </Box>
+        </TouchableRipple>
 
-			<TouchableRipple
-				borderless={true}
-				rippleColor='#99BBE3'
-				onPress={() => navigation.navigate('About')}>
-				<Box
-					p='2'
-					alignItems='center'
-					_text={{
-						fontSize: 'sm',
-						fontWeight: 'medium',
-						letterSpacing: 'lg',
-					}}>
-					<MaterialCommunityIcons name='lightbulb-on' size={24} />
-					{'About'}{' '}
-				</Box>
-			</TouchableRipple>
-		</HStack>
+        <TouchableRipple
+          borderless={true}
+          rippleColor="#99BBE3"
+          onPress={() => navigation.navigate("About")}
+        >
+          <Box
+            p="2"
+            alignItems="center"
+            _text={{
+              fontSize: "sm",
+              fontWeight: "medium",
+              letterSpacing: "lg",
+            }}
+          >
+            <MaterialCommunityIcons name="lightbulb-on" size={24} />
+            {"About"}{" "}
+          </Box>
+        </TouchableRipple>
+      </HStack>
     </Surface>
-	);
+  );
 };
 
 export default HomeNavBot;
+
+//create styleshee:
+const styles = StyleSheet.create({
+  container: {
+    width: screenWidth,
+    position: "absolute",
+    bottom: 0,
+    marginTop: 10,
+  },
+});
--- wlafrontend/screens/HomeScreen.tsx
+++ wlafrontend/screens/HomeScreen.tsx
@@ -1,151 +1,194 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect } from "react";
 import {
-	Text,
-	View,
-	RefreshControl,
-	ScrollView,
-	TouchableOpacity,
-	StyleSheet,
-	ImageBackground,
-} from 'react-native';
-import { Motion } from '@legendapp/motion';
-import { useQuery } from '@tanstack/react-query/build/lib';
-import { getHomeScreenContent } from '../api/index';
-import { useNavigation, NavigationProp } from '@react-navigation/native';
-import { useRefreshByUser } from '../hooks/useRefreshByUser';
-import {  useConnectivity } from '../hooks/useConnectivity';
-import SpinnerComp from '../components/Spinner';
-import OfflineToast from '../components/OfflineToast';
-import HomeNavBot from '../components/HomeNavBot';
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+import { Motion } from "@legendapp/motion";
+import { useQuery } from "@tanstack/react-query/build/lib";
+import { getHomeScreenContent } from "../api/index";
+import { useNavigation, NavigationProp } from "@react-navigation/native";
+import { useConnectivity } from "../hooks/useConnectivity";
+import SpinnerComp from "../components/Spinner";
+import OfflineToast from "../components/OfflineToast";
+import HomeNavBot from "../components/HomeNavBot";
+import HomeBackG from "../components/background/HomeBackG";
 
 type RootStackParamList = {
-	Home: undefined;
-	SendForHelp: undefined;
-	PublicMap: undefined;
-	About: undefined;
-	RescuerLogin: undefined;
-	Resources: undefined;
+  Home: undefined;
+  SendForHelp: undefined;
+  PublicMap: undefined;
+  About: undefined;
+  RescuerLogin: undefined;
+  Resources: undefined;
 };
-type HomeScreenNavigationProp = NavigationProp<RootStackParamList, 'Home'>;
+type HomeScreenNavigationProp = NavigationProp<RootStackParamList, "Home">;
 type Props = {
-	navigation: HomeScreenNavigationProp;
+  navigation: HomeScreenNavigationProp;
 };
+
+const screenHeight = Dimensions.get("window").height;
+const screenWidth = Dimensions.get("window").width;
 
 const Home = (props: Props) => {
-	const isConnected  =  useConnectivity();
-	const navigation = useNavigation<HomeScreenNavigationProp>();
+  const isConnected = useConnectivity();
+  const navigation = useNavigation<HomeScreenNavigationProp>();
 
-	const handlePress = async () => {
-		// Delay the navigation using setTimeout
-		await new Promise<void>((resolve) => {
-			setTimeout(() => {
-				resolve();
-			}, 300); // Adjust the delay
-		});
+  const handlePress = async () => {
+    // Delay the navigation
+    await new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 300);
+    });
 
-		navigation.navigate('SendForHelp');
-	};
+    navigation.navigate("SendForHelp");
+  };
 
-	useLayoutEffect(() => {
-		navigation.setOptions({
-			headerShown: false,
-		});
-	});
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
 
-	const { isLoading, refetch, data, error } = useQuery(
-    ['HomeScreen'],
+  const { isLoading, refetch, data, error } = useQuery(
+    ["HomeScreen"],
     () => getHomeScreenContent(),
     { enabled: isConnected }
   );
 
-	const { isRefetchingByUser, refetchByUser } = useRefreshByUser(refetch);
+  if (isLoading) {
+    return (
+      <View className="flex-1 align-middle justify-center">
+        <SpinnerComp />
+      </View>
+    );
+  }
 
-	if (isLoading || isRefetchingByUser) {
-		return <View className='flex-1 align-middle justify-center'><SpinnerComp /></View>;
-	}
+  if (error) {
+    return (
+      <View className="flex-1 align-middle justify-center">
+        <Text>{JSON.stringify(error)}</Text>;
+      </View>
+    );
+  }
 
-	if (error) {
-		return <View className='flex-1 align-middle justify-center'><Text>{JSON.stringify(error)}</Text>;</View>
-	}
-
-	return (
-		<ImageBackground
-			className='flex-1'
-			source={require('../assets/homescreen.png')}>
-			<ScrollView
-				refreshControl={
-					<RefreshControl
-						refreshing={isRefetchingByUser}
-						onRefresh={refetchByUser}
-					/>
-				}>
-				<Motion.View
-					initial={{ x: -300, scale: 0, opacity: 0 }}
-					animate={{ x: 0, scale: 1, opacity: 1 }}
-					transition={{
-						x: {
-							type: 'spring',
-							damping: 20,
-							stiffness: 800,
-							mass: 2,
-						},
-						opacity: {
-							type: 'tween',
-							duration: 1000,
-						},
-					}}>
-					<View className='mt-12 h-96 flex flex-col items-center'>
-						<View className=' mt-9 max-h-9  flex-1 py-1 px-4 align-middle justify-center  bg-[#77CFCA] rounded-xl'>
-							<Text className='font-bold text-lg text-[#24374b]'>
-								{data?.Message}
-							</Text>
-						</View>
-						<Text className='font-black mt-5 mix-blend-color  uppercase text-[#24374b] text-3xl'>
-							{data?.Title}
-						</Text>
-						<Text className=' text-2xl text-center px-4 text-[#24374b] font-light'>
-							{data?.Description}
-						</Text>
-					</View>
-					{isConnected? null : <OfflineToast />}
-					<View className='mt-11 flex-1 align-middle justify-center'>
-						<View className='flex flex-col items-center'>
-							<View className='flex-1 align-middle justify-center w-44 h-44 border border-spacing-10 border-[#15ff00fe] rounded-full flex flex-col items-center bg-[#009DAD]'>
-								<TouchableOpacity onPress={handlePress}>
-									<View style={styles.container}>
-										<Text className='text-[#2a527a] font-bold text-2xl'>
-											Start Alert
-										</Text>
-									</View>
-								</TouchableOpacity>
-							</View>
-						</View>
-					</View>
-				</Motion.View>
-			</ScrollView>
-			<View className='sticky bottom-0'>
-				<HomeNavBot navigation={navigation} />
-			</View>
-		</ImageBackground>
-	);
+  return (
+    <View style={styles.outer}>
+      <View style={styles.background}>
+        <HomeBackG />
+      </View>
+      <Motion.View
+        initial={{ x: -300, scale: 0, opacity: 0 }}
+        animate={{ x: 0, scale: 1, opacity: 1 }}
+        transition={{
+          x: {
+            type: "spring",
+            damping: 20,
+            stiffness: 800,
+            mass: 2,
+          },
+          opacity: {
+            type: "tween",
+            duration: 1000,
+          },
+        }}
+      >
+        <View className="mt-12 h-96 flex flex-col items-center">
+          <View className=" mt-9 max-h-9  flex-1 py-1 px-4 align-middle justify-center  bg-[#77CFCA] rounded-xl">
+            <Text className="font-bold text-lg text-[#24374b]">
+              {data?.Message}
+            </Text>
+          </View>
+          <Text className="font-black mt-5 mix-blend-color  uppercase text-[#24374b] text-3xl">
+            {data?.Title}
+          </Text>
+          <Text className=" text-2xl text-center px-4 text-[#24374b] font-light">
+            {data?.Description}
+          </Text>
+        </View>
+        {isConnected ? null : <OfflineToast />}
+        <View style={styles.holdingButton}>
+          <View style={styles.yellowcircle}>
+            <TouchableOpacity onPress={handlePress}>
+              <View style={styles.container}>
+                <Text className="text-[#2a527a] font-bold text-2xl">
+                  Start Alert
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Motion.View>
+      <View style={styles.nav}>
+        <HomeNavBot navigation={navigation} />
+      </View>
+    </View>
+  );
 };
 
 export default Home;
 
 const styles = StyleSheet.create({
-	container: {
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#15ff00',
-		margin: 6,
-		padding: 0,
-		height: 140,
-		width: 140,
-		borderRadius: 108,
-		shadowOffset: { width: 3, height: 4 },
-		shadowOpacity: 0.25,
-		shadowRadius: 4,
-		shadowColor: '#000',
-		innerShadow: 6,
-	},
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#15ff00",
+    margin: 6,
+    padding: 0,
+    height: 145,
+    width: 145,
+    borderRadius: 108,
+    shadowOffset: { width: 5, height: 6 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    shadowColor: "#000",
+    innerShadow: 6,
+  },
+  nav: {
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    top: 0,
+    width: screenWidth,
+    height: screenHeight,
+  },
+  background: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    top: 0,
+    right: 0,
+  },
+  outer: {
+    flex: 1,
+  },
+  yellowcircle: {
+    marginTop: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#04b6d1",
+    margin: 6,
+    padding: 0,
+    height: 180,
+    width: 180,
+    borderRadius: 108,
+    borderWidth: 2,
+    borderColor: "#15ff00fe",
+    shadowOffset: { width: 3, height: 4 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    shadowColor: "#000",
+  },
+  holdingButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 11,
+  },
 });
